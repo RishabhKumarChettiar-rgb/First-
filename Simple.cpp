32087927ef9ed0e36dfbc90204852ec4598a4137
RishabhKumarChettiar-rgb <9923103027@mail.jiit.ac.in>
2025-07-25 10:43:55 +0530

    Add files via upload

---
+++ Simple.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//Simple code printing numbers from 1 to 100.
+
+int main(){
+int number=1;
+
+while(number!=100){
+        number++;
+cout<<number<<endl;
+
+
+}
+
+
+
+return 0;
+}
